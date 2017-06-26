65f913881838bf4f159cd6642c11eb3681a9f3f4
jxh <jxh@zwzdeMacBook-Pro-2.local>
2017-06-26 11:04:47 +0800

    初始化

---
+++ BaiSiBuDeJie/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by jxh on 2017/6/22.
+//  Copyright © 2017年 JuXiaoHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiSiBuDeJie/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaiSiBuDeJie
+//
+//  Created by jxh on 2017/6/22.
+//  Copyright © 2017年 JuXiaoHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
